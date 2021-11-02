0c36764dfd2260508bf3910e18ad2cc6f583216b
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2021-11-02 17:53:24 +0700

    add project

---
+++ product.js
@@ -0,0 +1,45 @@
+export let product = [{
+    productId: 'P001',
+    productName: 'SHEIN Drawstring Ruched Side Crop Tank Top',
+    productDesc: 'Casual, Crop',
+    productPrice: '130 Baht',
+    productStock: '284 pieces'
+}, {
+    productId: 'P002',
+    productName: 'DAZY Plicated Detail Split Hem Tee Dress',
+    productDesc: 'Elegant ,Midi ,Summer',
+    productPrice: '310 Baht',
+    productStock: '89 pieces'
+}, {
+    productId: 'P003',
+    productName: 'DAZY Patch Detail Keyhole Back Blouse',
+    productDesc: 'Casual, Regular ,Spring/Fall',
+    productPrice: '198 Baht',
+    productStock: '94 pieces'
+}, {
+    productId: 'P004',
+    productName: 'DAZY Solid Ruched Crop Blouse',
+    productDesc: 'Boho, Ruched, Crop',
+    productPrice: '240 Baht',
+    productStock: '58 pieces'
+
+}, {
+    productId: 'P005',
+    productName: 'DAZY Puff Sleeve Textured Blouse',
+    productDesc: 'Casual ,Crop ,Spring/Fall',
+    productPrice: '250 Baht',
+    ProductStock: '70 pieces'
+
+}, {
+    productId: 'P006',
+    productName: 'Appliques Zipper Detail Cami Top',
+    productDesc: 'Appliques, Zipper',
+    productPrice: '129 Baht',
+    productStock: '99 pieces'
+}, {
+    productId: 'P007',
+    productName: 'SHEIN Floral Print Scallop Trim Cami Top',
+    productDesc: 'Style: Boho Color: Multicolor',
+    productPrice: '70 Baht',
+    productStock: '56 pieces'
+}]
